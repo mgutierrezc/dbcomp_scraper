7d29a0d646c2134831cf57daabf7778c4ead57ce
Marco Gutierrez <a20141676@pucp.pe>
2022-11-27 22:23:39 -0500

    updated scraper for new design

--- wscrapper_dbcomp.py
+++ wscrapper_dbcomp.py
@@ -19,87 +19,61 @@ chromeOptions = webdriver.ChromeOptions()
 path_1 = "D:\Descargas\chromedriver_win32 (2)\chromedriver.exe"
 driver = webdriver.Chrome(path_1)
 
-driver.get("https://db-comp.eu/")
+#driver.get("https://db-comp.eu/")
+years = [1990, 2011]
+for year in years:
 
-#Cerrar la wea de cookies
-driver.find_element(By.CLASS_NAME, "zenario_cc_accept").click()
+    driver.get("https://db-comp.eu/?slotName=Slot_Main_3&Search_by_value=details&Document_type_values=1&Month_from_value=1&Year_from_value={year}&Month_to_value=1&Year_to_value=2022&Mysql_operator_value=none&Sort_results_value=relevance&page=1")
+    
+    # TODO: extraer numero de paginas
+    num_pags = funcion_que_te_extrae_num_pags()
 
-#Darle click a agreement
-driver.find_element(By.ID, "1").click()
+    for n_pag in num_pags:
+        driver.get(f"https://db-comp.eu/?slotName=Slot_Main_3&Search_by_value=details&Document_type_values=1&Month_from_value=1&Year_from_value={year}&Month_to_value=1&Year_to_value=2022&Mysql_operator_value=none&Sort_results_value=relevance&page={n_pag}")
 
-#Introducir la fecha
+        #Cerrar la wea de cookies
+        driver.find_element(By.CLASS_NAME, "zenario_cc_accept").click()
 
-##### Mes de Inicio
-start_month = driver.find_element(By.ID, "plgslt_Slot_Main_3_month_from")
-start_month.click()
-start_month.send_keys(Keys.ARROW_DOWN)
+        #Darle click a agreement
+        driver.find_element(By.ID, "1").click()
 
-##### Año de Inicio
-start_year = driver.find_element(By.ID, "plgslt_Slot_Main_3_year_from")
-start_year.click()
-start_year.send_keys(Keys.ARROW_DOWN)
-start_year.send_keys(Keys.ARROW_DOWN)
-start_year.send_keys(Keys.ARROW_DOWN)
-start_year.send_keys(Keys.ARROW_DOWN)
-start_year.send_keys(Keys.ARROW_DOWN)
-start_year.send_keys(Keys.ARROW_DOWN)
+        #Introducir la fecha
 
-#### Mes de fin
-finish_month = driver.find_element(By.ID, "plgslt_Slot_Main_3_month_to")
-finish_month.click()
-finish_month.send_keys(Keys.ARROW_DOWN)
+        ##### Mes de Inicio
+        start_month = driver.find_element(By.ID, "plgslt_Slot_Main_3_month_from")
+        start_month.click()
+        start_month.send_keys(Keys.ARROW_DOWN)
 
-#### Año de fin
-finish_year = driver.find_element(By.ID, "plgslt_Slot_Main_3_year_to")
-finish_year.click()
-finish_year.send_keys(Keys.ARROW_DOWN)
+        ##### Año de Inicio
+        start_year = driver.find_element(By.ID, "plgslt_Slot_Main_3_year_from")
+        start_year.click()
+        start_year.send_keys(Keys.ARROW_DOWN)
+        start_year.send_keys(Keys.ARROW_DOWN)
+        start_year.send_keys(Keys.ARROW_DOWN)
+        start_year.send_keys(Keys.ARROW_DOWN)
+        start_year.send_keys(Keys.ARROW_DOWN)
+        start_year.send_keys(Keys.ARROW_DOWN)
 
+        #### Mes de fin
+        finish_month = driver.find_element(By.ID, "plgslt_Slot_Main_3_month_to")
+        finish_month.click()
+        finish_month.send_keys(Keys.ARROW_DOWN)
 
-#Darle click a search
-driver.find_element(By.ID, "plgslt_Slot_Main_3_search").click()
-time.sleep(1)
-# Dar click a una decisión, descargar, entrar a la otra pestaña, descargar, cerrar la pestaña, continuar con la que sigue, 
+        #### Año de fin
+        finish_year = driver.find_element(By.ID, "plgslt_Slot_Main_3_year_to")
+        finish_year.click()
+        finish_year.send_keys(Keys.ARROW_DOWN)
 
-# lnks = driver.find_elements(By.CSS_SELECTOR, '#plgslt_Slot_Main_3_results')
-#elems = driver.find_elements(By.TAG_NAME, 'a')
-page_source = driver.page_source
-with open("gaaaa.txt", "w+") as file:
-    file.write(page_source)
+        def scrape_links(driver, delay, xpath_results="//div[@id='plgslt_Slot_Main_3_results']//a[@href]", attribute_str="href"):            
+            lst = []
+            try:
+                elems = driver.find_elements(By.XPATH, xpath_results)
+                print("Page is ready!")
+                for elem in elems:
+                    print(elem.get_attribute(attribute_str))
+                    if elem.get_attribute(attribute_str) != None:
+                        lst.append(elem.get_attribute(attribute_str))
+            except TimeoutException:
+                print("Loading took too much time!")
 
-# body = driver.find_element(By.TAG_NAME, "body").text
-# time.sleep(5)
-delay = 5
-try:
-    #myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//div[@id='plgslt_Slot_Main_3_results']//a[@href]")))
-    elems = driver.find_elements(By.XPATH, "//div[@id='plgslt_Slot_Main_3_results']//a[@href]")
-    print("Page is ready!")
-    for elem in elems:
-        print(elem.get_attribute("href"))
-except TimeoutException:
-    print("Loading took too much time!")
-
-# soup = BeautifulSoup(page_source, 'html.parser')
-# divs = soup.find_all("result")
-# for div in divs:
-#     print("div: ", div)
-
-# links = []
-# links_selector = soup.find_all('div', class_='result')
-# print("links_selector: ", links_selector)
-
-
-# elems = driver.find_elements_by_xpath("//a[@href]")
-# for link in elems:
-#     print("link: ", link.get_attribute("href"))
-
-
-# print("link: ", lnks[0])
-# print("link: ", len(lnks))
-
-# for lnk in lnks:
-#     print("link with href: ", lnk.get_attribute("href"))
-
-# Descargar las decisiones en una carpeta
-
-# Cuando no hayan más decisiones, darle a siguiente página
-# driver.close()
+            return lst[:len(lst)-2]
